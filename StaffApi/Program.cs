0ada4ae625a9d1ace99ef6b8c9106cafc5e1eea0
jonathonchilds <33766594+jonathonchilds@users.noreply.github.com>
2022-09-08 16:32:13 -0400

    updated namespace in "program.cs" file

--- StaffApi/Program.cs
+++ StaffApi/Program.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
-namespace staffapi
+namespace John_Smith_Watched_Movies_2021
 {
     public class Program
     {
